179ed2688bcf113b9b5f42740097cba851b91f3f
aj <khalilajstunner@gmail.com>
2018-12-05 22:44:57 +0300

    Static files update

--- conference_api/urls.py
+++ conference_api/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from conference_api import settings
 
 urlpatterns = [
-    url(r"^admin/", admin.site.urls, 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    url(r"^admin/", admin.site.urls),
     url(r"^", include("app.urls")),
 ]

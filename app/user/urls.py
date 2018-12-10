189d979d89f94379d5b4896ed5fe8e4e5f6314aa
aj <khalilajstunner@gmail.com>
2018-12-10 06:12:02 +0300

    user urls updated

--- app/user/urls.py
+++ app/user/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from . import views
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     url(r"^$", views.AccountRetrieveUpdateView.as_view()),
     url(r"^(?P<pk>\d+)$", views.AccountEdit.as_view(), name="edit-user"),

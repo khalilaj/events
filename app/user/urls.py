3d94f13798cc629f2442f00169db9e674566cb6f
aj <khalilajstunner@gmail.com>
2018-12-10 06:06:45 +0300

    user urls updated

--- app/user/urls.py
+++ app/user/urls.py
@@ -4,4 +4,4 @@ from . import views
 urlpatterns = [
     url(r"^$", views.AccountRetrieveUpdateView.as_view()),
     url(r"^(?P<pk>\d+)$", views.AccountEdit.as_view(), name="edit-user"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
